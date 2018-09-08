611fbbb70cc719e2a58e0c83a68a96c7bf1990af
Domenic Denicola <d@domenic.me>
2018-09-08 18:10:39 -0400

    Extract out API functions from Real Numbers to a helper script
    This will allow them to be reused in upcoming work.

---
+++ helpers/api.js
@@ -0,0 +1,45 @@
+"use strict";
+const API_KEY_LENGTH = 32;
+
+async function getAPIData(path) {
+  const apiKey = await getAPIKey();
+
+  const res = await fetch(`/api/user/${apiKey}/${path}`);
+  if (!res.ok) {
+    localStorage.removeItem("apiKey");
+    throw new Error(`API call resulted in ${res.status} status`);
+  }
+  const json = await res.json();
+
+  if (json.error) {
+    throw new Error("API error: " + json.error.message);
+  }
+
+  return json.requested_information;
+}
+
+async function getAPIKey() {
+  const apiKeyFromStorage = localStorage.getItem("apiKey");
+  if (apiKeyFromStorage) {
+    return apiKeyFromStorage;
+  }
+
+  console.debug("[WK real numbers] API key not stored; fetching it.");
+  const accountRes = await fetch("/settings/account");
+  if (!accountRes.ok) {
+    throw new Error(`Fetching the account page gave a ${accountRes.status} status`);
+  }
+  const accountDocument = parseDocument(await accountRes.text());
+  const apiKey = accountDocument.querySelector("#user_api_key").value;
+
+  if (typeof apiKey !== "string" || apiKey.length !== API_KEY_LENGTH) {
+    throw new Error("Failed to get API key from the account page");
+  }
+
+  localStorage.setItem("apiKey", apiKey);
+  return apiKey;
+}
+
+function parseDocument(string) {
+  return (new DOMParser()).parseFromString(string, "text/html");
+}
--- scripts/real-numbers.js
+++ scripts/real-numbers.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        WaniKani Real Numbers
 // @namespace   https://github.com/domenic/wk-scripts
-// @version     1.1.0
+// @version     1.1.1
 // @author      Domenic Denicola
 // @description Shows the real number of lessons and reviews, instead of 42+
 // @license     MIT
@@ -9,6 +9,7 @@
 // @downloadURL https://raw.githubusercontent.com/domenic/wk-scripts/master/scripts/real-numbers.js
 // @updateURL   https://raw.githubusercontent.com/domenic/wk-scripts/master/scripts/real-numbers.js
 // @supportURL  https://github.com/domenic/wk-scripts/issues
+// @require     https://raw.githubusercontent.com/domenic/wk-scripts/master/helpers/api.js
 // @match       https://www.wanikani.com/*
 // @run-at      document-end
 // @grant       none
@@ -17,7 +18,6 @@
 "use strict";
 
 const FAKE_NUMBER = "42+";
-const API_KEY_LENGTH = 32;
 
 (async () => {
   const lessonsEl = document.querySelector(".lessons span");
@@ -42,59 +42,12 @@ const API_KEY_LENGTH = 32;
   reviewsEl.textContent = "";
 
   try {
-    const apiKey = await getAPIKey();
-    console.debug(`[WK real numbers] API key is ${apiKey}.`);
-
-    const { lessons, reviews } = await getData(apiKey);
-    lessonsEl.textContent = lessons;
-    reviewsEl.textContent = reviews;
+    const studyQueue = await getAPIData("study-queue");
+    lessonsEl.textContent = studyQueue.lessons_available;
+    reviewsEl.textContent = studyQueue.reviews_available;
   } catch (e) {
     console.error("[WK real numbers] " + e.message);
     lessonsEl.textContent = lessonsBefore;
     reviewsEl.textContent = reviewsBefore;
   }
 })();
-
-async function getData(apiKey) {
-  const res = await fetch(`/api/user/${apiKey}/study-queue`);
-  if (!res.ok) {
-    localStorage.removeItem("apiKey");
-    throw new Error(`API call resulted in ${res.status} status`);
-  }
-  const json = await res.json();
-
-  if (json.error) {
-    throw new Error("API error: " + json.error.message);
-  }
-
-  return {
-    lessons: json.requested_information.lessons_available,
-    reviews: json.requested_information.reviews_available
-  };
-}
-
-async function getAPIKey() {
-  const apiKeyFromStorage = localStorage.getItem("apiKey");
-  if (apiKeyFromStorage) {
-    return apiKeyFromStorage;
-  }
-
-  console.debug("[WK real numbers] API key not stored; fetching it.");
-  const accountRes = await fetch("/settings/account");
-  if (!accountRes.ok) {
-    throw new Error(`Fetching the account page gave a ${accountRes.status} status`);
-  }
-  const accountDocument = parseDocument(await accountRes.text());
-  const apiKey = accountDocument.querySelector("#user_api_key").value;
-
-  if (typeof apiKey !== "string" || apiKey.length !== API_KEY_LENGTH) {
-    throw new Error("Failed to get API key from the account page");
-  }
-
-  localStorage.setItem("apiKey", apiKey);
-  return apiKey;
-}
-
-function parseDocument(string) {
-  return (new DOMParser()).parseFromString(string, "text/html");
-}
